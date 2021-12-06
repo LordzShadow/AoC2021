3808dbb6e5d3bcf55dcc2a7248341fd0685b42f3
Kustav Prants <kustav.prants@mil.ee>
2021-12-06 09:44:49 +0200

    Day 6

--- day6/lanternfish.py
+++ day6/lanternfish.py
@@ -1,15 +1,22 @@
-
-fishies = []
-n = 80
-
-def update(fish):
-	if fish == 0:
-		return 6
-	else:
-		return fish-1
+counter = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
+n = 256
 
 with open("input.txt", "r") as file:
-	fishies = list(map(int, file.readline.strip().split()))
+	for fish in file.readline().strip().split(","):
+		counter[int(fish)] += 1
 
 for i in range(n):
+	temp_c = dict(counter)
+	for j in range(9):
+		if j == 0:
+			temp_c[8] += counter[j]
+			temp_c[6] += counter[j]
+		else:
+			temp_c[j-1] += counter[j]
+		temp_c[j] -= counter[j]
+	counter = dict(temp_c)
 
+sum = 0
+for i in range(9):
+	sum +=  counter[i]
+print(sum)
