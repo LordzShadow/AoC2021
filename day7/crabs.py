e9ee83ff2c11c8894c3e16b68205fdcbba605745
LordzShadow <kustav.prants@gmail.com>
2021-12-07 11:04:34 +0200

    Day 7

---
+++ day7/crabs.py
@@ -0,0 +1,17 @@
+import statistics, math
+crabs = []
+
+def part1(crab, i):
+	return abs(crab-i)
+
+def part2(crab, i):
+	return (part1(crab, i) * (part1(crab, i) + 1)) // 2
+
+with open("input.txt", "r") as file:
+	crabs = list(map(int, file.readline().split(",")))
+	minimum = min(crabs)
+	maximum = max(crabs)
+	values = min([sum([part1(crab, i) for crab in crabs]) for i in range(minimum, maximum+1)])
+	print(values)
+	values = min([sum([part2(crab, i) for crab in crabs]) for i in range(minimum, maximum+1)])
+	print(values)
